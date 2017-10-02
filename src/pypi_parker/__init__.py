e6f29b25e64ec62e544f0541308bcc9d862f8b15
Matt <matt.s.b.42@gmail.com>
2017-10-02 04:13:59 +0000

    bumping version to 0.1.0 for pypi release

--- src/pypi_parker/__init__.py
+++ src/pypi_parker/__init__.py
@@ -7,7 +7,7 @@ import sys
 from pypi_parker.build import generate_and_build_package
 from pypi_parker.config import load_config
 
-__version__ = '0.0.28'
+__version__ = '0.1.0'
 
 
 class Park(Command):

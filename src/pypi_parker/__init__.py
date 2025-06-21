28ac0515be8aada15098462d1eff3088716074b8
Matt Bullock <m@ttsb42.com>
2025-06-21 12:13:24 -0700

    feat: convert to from tox to uv and from distutils to cli

--- src/pypi_parker/__init__.py
+++ src/pypi_parker/__init__.py
@@ -1,39 +1,33 @@
 """PyPI Parker setup expansion resources."""
-# virtualenv + distutils bug in pylint: https://github.com/PyCQA/pylint/issues/73
-from distutils.cmd import Command  # pylint: disable=import-error,no-name-in-module
-import os
+import importlib.metadata
 import sys
+import argparse
+from pathlib import Path
 
 from pypi_parker.build import generate_and_build_package
 from pypi_parker.config import load_config
 
-__version__ = '0.1.2'
+__version__ = importlib.metadata.version(__name__)
 
 
-class Park(Command):
-    """Distutils Command for reading a :class:`pypi-parker` configuration
-    and building distribution files for each name.
-    """
+def cli() -> None:
+    parser = argparse.ArgumentParser(
+        description='Generate placeholder packages from pypi-parker configuration.',
+    )
+    parser.add_argument(
+        '-f', '--config-file',
+        type=Path,
+        default='park.cfg',
+        required=False,
+        help='Path to pypi-parker configuration file',
+    )
+    parser.add_argument('--version', action='version', version=__version__)
 
-    decription = 'Generates builds for all configured PyPI names'
-    user_options = [
-        ('park-config=', 'p', 'path to pypi-parker config file')
-    ]
+    args = parser.parse_args(sys.argv[1:])
 
-    def initialize_options(self) -> None:
-        """Set default values for options."""
-        self.park_config = 'park.cfg'  # pylint: disable=attribute-defined-outside-init
+    if not args.config_file.is_file():
+        parser.error('Configuration file "{}" does not exist'.format(args.config_file))
 
-    def finalize_options(self) -> None:  # noqa=D401
-        """Required by :class:`distutils.cmd` but not used."""
-        self.park_config = os.path.abspath(self.park_config)  # pylint: disable=attribute-defined-outside-init
-
-        if not os.path.isfile(self.park_config):
-            sys.exit('Requested pypi_parker config file "{}" does not exist'.format(self.park_config))
-
-    def run(self) -> None:
-        """Run command."""
-        base_dir = os.path.dirname(self.park_config)
-
-        for package in load_config(self.park_config):
-            generate_and_build_package(package, base_dir)
+    base_dir = args.config_file.parent.resolve()
+    for package in load_config(args.config_file.resolve()):
+        generate_and_build_package(package, base_dir)
